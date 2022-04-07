9ec3a1f20b6024a4c797a716a65b7b3c47626088
Zuhdi <zuhdi.flutter@gmail.com>
2022-04-08 01:08:11 +0800

    rev3

--- events.py
+++ events.py
@@ -1,7 +1,8 @@
-import multiprocessing 
+import multiprocessing
 from Event2_sonar import *
 from Event1_Battery import *
 
-urgency = 0
+urgency_level = 0
 
-readDist = 
+readDist = multiprocessing.Process(target=ObstacleCase)
+readBatt = multiprocessing.Process(target=BatteryCase)
